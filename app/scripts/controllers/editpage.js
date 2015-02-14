6dd046ee2071c067d3ce50dc4ac0defe91d5de7c
Jesus <ctw@ctwhome.com>
2015-02-14 10:04:23 +0100

    saving data to db, work in progress

--- app/scripts/controllers/editpage.js
+++ app/scripts/controllers/editpage.js
@@ -10,14 +10,36 @@
 angular.module('App')
   .controller('EditpageCtrl',
     function ($rootScope, $scope, $http) {
+        //https://api.mongolab.com/api/1/databases/styleguide/collections/content?q={"active": true}&fo=true&apiKey=myAPIKey
 
-        $http.get('https://api.mongolab.com/api/1/databases/styleguide/collections/content?apiKey=9xkyuiXnGhk_EzhreL1uLaPqIoIIkOxo').
+
+        var id = "second",
+            apikey = "9xkyuiXnGhk_EzhreL1uLaPqIoIIkOxo",
+            url = 'https://api.mongolab.com/api/1/databases/styleguide/collections/content?f={"_id":0, "id":0}&q={"id":"'+id+'"}&apiKey='+apikey;
+
+        $scope.data = {
+            Content: $scope.content,
+            //title: $scope.content[0].title,
+            //"menu-title": $scope.content[0].menu-title,
+            id: id
+        }
+
+        $scope.content = "Loading content";
+
+        /**
+         * Load data
+         */
+        $http.get(url).
             success(function(data) {
-                $scope.content = data[0].content;
+                $scope.content = data;
+                console.log(data);
             });
 
-                $scope.content = "Loading content";
 
+        /**
+         * Open Editor
+         * @type {Array}
+         */
         $scope.ckEditors = [];
         $scope.addEditor = function(){
 
@@ -25,4 +47,21 @@ angular.module('App')
         }
 
 
-  });
+        /**
+         * Save data
+         */
+        $scope.saveData = function(){
+            console.log($scope.data);
+            $http.put(url, $scope.data).
+                success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    console.log("Data saved");
+                }).
+                error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    console.log(data);
+                });
+        }
+    });
